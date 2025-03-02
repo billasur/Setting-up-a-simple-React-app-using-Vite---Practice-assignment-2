75c3bdd4d7ac215c06c12e7aecfde4dbba6410dc
Prahalad Singh Gaur <singhgaurprahalad@gmail.com>
2025-03-03 02:27:05 +0530

    Setting up a simple React app using Vite - Practice assignment #2

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>
+          Prahald Singh Gaur
+        </h1>
+        <h2>
+          <a href='mailto:prahalad.singh.s70@kalvium.community?subject=Contacting&body=Hello, I want to contact you about.. '>
+          Contact me
+          </a>
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
